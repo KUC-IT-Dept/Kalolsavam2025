e9adbf7f6728f012bfe420c843ddd6320524f3ba
shad <contactshadct@gmail.com>
2025-02-06 07:48:08 +0530

    completed Schedule page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,69 +2,34 @@ import logo from '../assets/logo.png';
 import NavBar from './navBar';
 
 const data = [
-  {
-    id: 1,
-    name: "Team A",
-    score: 10,
-    position: 0
-  },
-  {
-    id: 2,
-    name: "Team B",
-    score: 20,
-    position: 0
-  },
-  {
-    id: 3,
-    name: "Team C",
-    score: 30,
-    position: 0
-  },
-  {
-    id: 4,
-    name: "Team D",
-    score: 30,
-    position: 0
-  },
-  {
-    id: 5,
-    name: "Team E",
-    score: 30,
-    position: 0
-  },
-  {
-    id: 6,
-    name: "Team F",
-    score: 90,
-    position: 0
-  }
+  { id: 1, name: "Team A", score: 10, position: 0 },
+  { id: 2, name: "Team B", score: 20, position: 0 },
+  { id: 3, name: "Team C", score: 30, position: 0 },
+  { id: 4, name: "Team D", score: 30, position: 0 },
+  { id: 5, name: "Team E", score: 30, position: 0 },
+  { id: 6, name: "Team F", score: 90, position: 0 }
 ];
 
 function Home() {
-  // sort the data 
   const sortedData = data.sort((a, b) => b.score - a.score);
-
-  // Assign positions based on the sorted order
   sortedData.forEach((team, index) => {
-    team.position = index + 1; // Position starts from 1
+    team.position = index + 1;
   });
 
   return (
-    <div className="bg-[#F6E7D8] h-screen flex flex-col items-center">
-      <img src={logo} className='w-[10vh] mt-[-3vh] ' alt="Logo" />
-
+    <div className="bg-[#F6E7D8] flex flex-col items-center flex-grow">
+      <img src={logo} className='w-[10vh] mt-[-3vh]' alt="Logo" />
       <h1 className="text-4xl font-bold text-[#874356] mt-[-3vh] mb-3">സകവാഴ്‌വ്</h1>
-      <div className="w-full max-w-md ">
+      <div className="w-full max-w-md">
         {sortedData.map((team) => (
-          <div key={team.id} className="bg-[#874356] ml-5 mr-5  text-white p-4 mb-4 rounded-lg">
+          <div key={team.id} className="bg-[#874356] ml-5 mr-5 text-white p-4 mb-4 rounded-lg">
             <h2 className="text-xl">{team.name}</h2>
             <p>Total Score: <span className="font-semibold">{team.score}</span></p>
             <p>Position: <span className="font-semibold">{team.position}</span></p>
           </div>
         ))}
-
-        <NavBar/>
       </div>
+      <NavBar />
     </div>
   );
 }
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import NavBar from './navBar'
+import logo from '../assets/logo.png';
+import NavBar from './navBar';
+
+const Day1 = [
+  { id: 1, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 2, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 3, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 4, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 5, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 6, name: "Program A", Venue: "IT Dept", Time: "10 PM" }
+];
+
+const Day2 = [
+  { id: 1, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 2, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 3, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 4, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 5, name: "Program A", Venue: "IT Dept", Time: "10 PM" },
+  { id: 6, name: "Program A", Venue: "IT Dept", Time: "10 PM" }
+];
 
 function Schedule() {
   return (
-    <>
-        <h1>Schedule</h1>
-        <NavBar/>
-    </>
-  )
+    <div className="bg-[#F6E7D8] flex flex-col items-center flex-grow">
+      <img src={logo} className='w-[10vh] mt-[-3vh]' alt="Logo" />
+      <h1 className="text-4xl font-bold text-[#874356] mt-[-3vh] mb-3">Schedule</h1>
+
+      <div className="bg-[#F68989] ml-5 mr-5 text-white p-3 mb-4 rounded-lg">10/06/2025</div>
+
+      <div className="w-full max-w-md">
+        {Day1.map((team) => (
+          <div key={team.id} className="bg-[#874356] ml-5 mr-5 text-white p-3 mb-4 rounded-lg">
+            <h2 className="text-xl">{team.name}</h2>
+            <p>Venue: <span className="font-semibold">{team.Venue}</span></p>
+            <p>Time: <span className="font-semibold">{team.Time}</span></p>
+          </div>
+        ))}
+      </div>
+
+
+      <div className="bg-[#F68989] ml-5 mr-5 text-white p-3 mb-4 rounded-lg">10/06/2025</div>
+
+      <div className="w-full max-w-md">
+        {Day1.map((team) => (
+          <div key={team.id} className="bg-[#874356] ml-5 mr-5 text-white p-3 mb-4 rounded-lg">
+            <h2 className="text-xl">{team.name}</h2>
+            <p>Venue: <span className="font-semibold">{team.Venue}</span></p>
+            <p>Time: <span className="font-semibold">{team.Time}</span></p>
+          </div>
+        ))}
+      </div>
+
+      <NavBar />
+      <div className='h-[30vh]'></div>
+    </div>
+  );
 }
 
-export default Schedule
+export default Schedule;
